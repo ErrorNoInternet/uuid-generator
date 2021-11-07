3ed3e0cecd53d496036b13e1770738d27a94c216
Ryan <error.nointernet@gmail.com>
2021-11-07 16:43:50 +0800

    Fix bugs

--- uuidgenerator.java
+++ uuidgenerator.java
@@ -1,39 +1,40 @@
 import java.util.*;
 
-public class uuidgenerator{
-    public static String generateUUID(){
+public class uuidgenerator {
+    public static String generateUUID() {
         String uuid = "";
         Random random = new Random();
         String[] characters = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
         
-        for(int i=1; i<9; i++){
+        for (int i = 1; i < 9; i++) {
             int randomIndex = random.nextInt(characters.length);
             uuid += characters[randomIndex];
         }
 
         uuid += "-";
-        for(int i=1; i<4; i++){
-            for(int x=1; x<5; x++){
+        for (int i = 1; i < 4; i++) {
+            for (int x = 1; x < 5; x++) {
                 int randomIndex = random.nextInt(characters.length);
                 uuid += characters[randomIndex];
             }
             uuid += "-";
         }
 
-        for(int i=1; i<13; i++){
+        for (int i = 1; i < 13; i++) {
             int randomIndex = random.nextInt(characters.length);
             uuid += characters[randomIndex];
         }
 
         return uuid;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] arguments) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Number of UUIDs: ");
         int count = input.nextInt();
 
-        for(int i=0; i<count; i++){
+        for (int i = 0; i < count; i++) {
             System.out.println(generateUUID());
         }
     }
